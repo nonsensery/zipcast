e324a47ea72a5c7ad0d5d571823e77bcef2558e6
nonsensery <ajohnson@quickleft.com>
2015-01-27 16:59:23 -0800

    Add current conditions from Forecast.io

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,35 @@ var express = require('express');
 var router = express.Router();
 
 var zipcodes = require('zipcodes');
+var Forecast = require('forecast.io');
+
+
+var forecast = new Forecast({
+  APIKey: process.env.FORECAST_API_KEY,
+  timeout: 1000
+});
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Zipcast' });
 });
 
-router.get('/:zip', function (req, res) {
+router.get('/:zip', function (req, res, next) {
   zip = req.params.zip;
 
   // Synchronous lookup... boo!
   info = zipcodes.lookup(zip);
   info.zip = zip;
 
-  res.render('weather', { title: 'Zipcast', zipInfo: info });
+  forecast.get(info.latitude, info.longitude, function (forecastErr, forecastRes, forecastData) {
+    if (forecastErr) {
+      next(forecastErr);
+      return;
+    }
+
+    res.render('weather', { title: 'Zipcast', zipInfo: info, forecastData: forecastData });
+  });
 });
 
 module.exports = router;
